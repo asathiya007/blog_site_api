27aadd6f511b43228e6ca37397871195116155e6
asathiya007 <akshay.sathiya@gmail.com>
2020-08-26 18:32:24 -0700

    connect to MongoDB Atlas

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require('express');
+require('dotenv').config();
+const mongoose = require('mongoose');
 
 const app = express(); 
 
 app.use(express.json());
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true, 
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        });
+        console.log('MongoDB connected');
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+}
+connectDB();
 
 app.get('/test', (req, res) => {
     res.json("Hello world!");
